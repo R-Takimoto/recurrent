cfa34b852f47a6d966f43ea4296872d0a6950994
R-Takimoto <athlete_0818@yahoo.co.jp>
2020-11-30 11:02:34 +0900

    dao

--- recurrent_OKFTJ/src/dao/MenurDAO.java
+++ recurrent_OKFTJ/src/dao/MenurDAO.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 
 public class MenurDAO {
 	 // データベース接続に使用する情報
-	  private final String JDBC_URL = "jdbc:mysql://192.168.10.14:3306/recurrent?characterEncoding=UTF-8&serverTimezone=JST";
-	  private final String DB_USER = "root";
-	  private final String DB_PASS = "root";
+	  private final String JDBC_URL = "jdbc:mysql://192.168.10.18:3306/recurrent?characterEncoding=UTF-8&serverTimezone=JST";
+	  private final String DB_USER = "recurrent";
+	  private final String DB_PASS = "0000";
 
 	  public void findAll() {
 
@@ -19,7 +19,7 @@ public class MenurDAO {
 	        JDBC_URL, DB_USER, DB_PASS)) {
 
 	      // SELECT文を準備
-	      String sql = "SELECT typeCode FROM product LIMIT 0,1";
+	      String sql = "SELECT typecode FROM product ";
 	      PreparedStatement pStmt = conn.prepareStatement(sql);
 //	      pStmt.setString(1, login.getUserId());
 //	      pStmt.setString(2, login.getPass());
@@ -30,7 +30,7 @@ public class MenurDAO {
 	      // そのユーザーを表すAccountインスタンスを生成
 	      if (rs.next()) {
 	        // 結果表からデータを取得
-	        String code = rs.getString("typeCode");
+	        String code = rs.getString("typecode");
 	        System.out.println(code);
 	      }
 	    } catch (SQLException e) {
