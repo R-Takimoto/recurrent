16023723d051106110686e7b8addcb3d3a9782dd
R-Takimoto <athlete_0818@yahoo.co.jp>
2020-12-02 14:23:28 +0900

    Energy編集しました。

--- recurrent_OKFTJ/src/servlet/EnergyServlet.java
+++ recurrent_OKFTJ/src/servlet/EnergyServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bo.EnergyLogic;
 import model.Energy;
 
 
@@ -25,27 +27,43 @@ public class EnergyServlet extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//エネルギー計算画面よりフォーム入力値受け取り
-		request.setCharacterEncoding("utf-8");
-		String gender=request.getParameter("gender");
-		String age=request.getParameter("age");
-		String level=request.getParameter("level");
-		//エネルギー結果画面に移動するためにフォームaction属性を受け取りリクエストスコープに保存
-		String action=request.getParameter("action");
-		request.setAttribute("action", action);
+		//入力内容の引継ぎ(折出)
+		{
+			//エネルギー計算画面よりフォーム入力値受け取り
+			request.setCharacterEncoding("utf-8");
+			String gender=request.getParameter("gender");
+			String age=request.getParameter("age");
+			String level=request.getParameter("level");
+			//エネルギー結果画面に移動するためにフォームaction属性を受け取りリクエストスコープに保存
+			String action=request.getParameter("action");
+			request.setAttribute("action", action);
 
-		System.out.println(gender);
-		System.out.println(age);
-		System.out.println(level);
+			//受け取った入力値（文字列）を数値に変換
+			int ageId=Integer.parseInt(age);
+			int genderId=Integer.parseInt(gender);
+			int levelId=Integer.parseInt(level);
 
-		//受け取った入力値（文字列）を数値に変換
-		int ageId=Integer.parseInt(age);
-		int genderId=Integer.parseInt(gender);
-		int levelId=Integer.parseInt(level);
+			//結果画面に入力値を戻すためにエネルギークラス使用してリクエストスコープに保存
+			Energy energy=new Energy(ageId,genderId,levelId);
+			request.setAttribute("Energy", energy);
+		}
 
-		//結果画面に入力値を戻すためにエネルギークラス使用してリクエストスコープに保存
-		Energy energy=new Energy(ageId,genderId,levelId);
-		request.setAttribute("Energy", energy);
+		//必要エネルギー量の取得(滝本)
+		{
+			int ageId = Integer.parseInt(request.getParameter("age"));
+			int genderId = Integer.parseInt(request.getParameter("gender"));
+			int levelId = Integer.parseInt(request.getParameter("level"));
+			Energy energy = new Energy(ageId, genderId, levelId);
+
+			EnergyLogic energyL = new EnergyLogic();
+			energyL.execute(energy);
+
+			HttpSession session = request.getSession();
+			session.setAttribute("energy", energy);
+		}
+
+
+
 
 		//エネルギー結果画面にフォワード
 		RequestDispatcher dispatcher=request.getRequestDispatcher("/WEB-INF/jsp/energy.jsp");
