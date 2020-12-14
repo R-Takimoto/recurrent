f2c809f1333b1479730df2c6a48082b293d9d460
DESKTOP-670T9FU\S1-04 <ai.melty.1234@gmail.com>
2020-12-14 10:25:23 +0900

    会計

--- recurrent_OKFTJ/src/servlet/SlipServlet.java
+++ recurrent_OKFTJ/src/servlet/SlipServlet.java
@@ -9,21 +9,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bo.SlipLogic;
+import model.Slips;
+import model.Terminal;
 
 @WebServlet("/SlipServlet")
 public class SlipServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//    	request.setCharacterEncoding("utf-8");
-    	String action= request.getParameter ("action");
 
-		if(action==null) {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//    	request.setCharacterEncoding("utf-8");
+		String action = request.getParameter("action");
+
+		SlipLogic sl = new SlipLogic();
+		Terminal terminal = new Terminal(50);
+
+		SlipLogic lo = new SlipLogic();
+
+		Slips slips = lo.execute(terminal);
+		request.setAttribute("kaikei", slips);
+		//		request.setAttribute("Product", slips.getSlips().get(0).getProduct());
+		//		request.setAttribute("Quantity", slips.getSlips().get(0).getQuantity());
+		//		request.setAttribute("Price", slips.getSlips().get(0).getPrice());
+		//		request.setAttribute("Calorie", slips.getSlips().get(0).getCalorie());
+		//		ArrayList<String> kaikei = new ArrayList<String>();
+		//		kaikei.add("Product");
+		//		kaikei.add("Quantity");
+		//		kaikei.add("Price");
+		//		kaikei.add("Calorie");
+
+		//		System.out.println(kaikei.get(0));
+
+		if (action == null) {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/slip.jsp");
 			dispatcher.forward(request, response);
-    	}else {
-    		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/end.jsp");
-        	dispatcher.forward(request, response);
+		} else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/end.jsp");
+			dispatcher.forward(request, response);
 
-    	}
+		}
 	}
 }
